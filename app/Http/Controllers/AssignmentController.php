cd22efd74142649aa2f1a93805a19671420320d7
Arif Hassan <banyaiub@gmail.com>
2023-05-22 21:25:03 +0600

    first commit

---
+++ app/Http/Controllers/AssignmentController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+
+
+class AssignmentController extends Controller
+
+{// question-1
+    function UserInfo(Request $request):string{
+        $name = $request->input('name');
+        return $name; 
+}
+       // question-2
+       function UserAgent(Request $request):string{
+        $userAgent = $request->header('User-Agent');
+        return $userAgent;
+    }
+
+    
+    // question-3
+    function ApiEndpoint(Request $request){
+        $page = $request->query('page',null);
+        if($page !== null){
+            return $page;
+        }else{
+            return;
+        }
+
+
+    }
+
+
+        // question-4
+        function JsonResponse():JsonResponse{
+            $data = array(
+                "message"=> "Success",
+                "data"=>array(
+                    "name"=> "John Doe",
+                    "age"=> 25
+                )
+            );
+            return response()->json($data);
+        }
+
+
+
+           // question-5
+    function FileUploads(Request $request):bool{
+        $file=$request->file('avatar');
+        $file->move(public_path('uploads'),$file->getClientOriginalName());
+        return true;
+    }
+
+    function SetCookie(Request $request){
+        $rememberToken = $request->cookie('remember_token',null);
+        return $rememberToken;
+    }
+
+}
